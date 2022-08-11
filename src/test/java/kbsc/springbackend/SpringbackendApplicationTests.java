eca221ca414bc379c96e3bae0ecd7136885862cb
HwangJongWon <ccccc1088@naver.com>
2022-08-11 19:16:45 +0900

    Initial commit

---
+++ src/test/java/kbsc/springbackend/SpringbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kbsc.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
